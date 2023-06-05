6732b1be3d6d590f5ded9bb5bae9c1a26e195dea
Bryan Yang <41586021+BryanDYang@users.noreply.github.com>
2023-06-05 10:23:53 -0700

    Task 2 Complete

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -14,7 +14,7 @@ interface IProps {
  * Perspective library adds load to HTMLElement prototype.
  * This interface acts as a wrapper for Typescript compiler.
  */
-interface PerspectiveViewerElement {
+interface PerspectiveViewerElement extends HTMLElement {
   load: (table: Table) => void,
 }
 
@@ -32,7 +32,7 @@ class Graph extends Component<IProps, {}> {
 
   componentDidMount() {
     // Get element to attach the table from the DOM.
-    const elem: PerspectiveViewerElement = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
+    const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
 
     const schema = {
       stock: 'string',
@@ -46,9 +46,26 @@ class Graph extends Component<IProps, {}> {
     }
     if (this.table) {
       // Load the `table` in the `<perspective-viewer>` DOM reference.
-
-      // Add more Perspective configurations here.
+      
+      // Adding attribute to perspective-viewer elements
       elem.load(this.table);
+      
+      // Set view to y_line
+      elem.setAttribute('view', 'y_line');
+
+      // Set column-pivots to stock (ABC or DEF)
+      elem.setAttribute('column-pivots', '["stock"]')
+      
+      // Set row-pivots to timestamp
+      elem.setAttribute('row-pivots', '["timestamp"]')
+      
+      // Set columns to top_ask_price
+      elem.setAttribute('columns', '["top_ask_price"]')
+
+      // Aggregates to avoid dupated data
+      elem.setAttribute('aggregates', 
+      '{"stock":"distinct count", "top_ask_price":"avg", "top_bid_price":"avg", "timestamp":"dinstinct count"}');
+      
     }
   }
 
